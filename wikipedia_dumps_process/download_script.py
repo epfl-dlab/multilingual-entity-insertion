f81100c4c02203f1c6510740cdc66f344d4a3dcc
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2023-10-02 16:58:41 +0200

    fix processing pipeline to use XML dumps too

--- wikipedia_dumps_process/download_script.py
+++ wikipedia_dumps_process/download_script.py
@@ -3,7 +3,8 @@ import requests
 import urllib.request
 from tqdm import tqdm
 import os
-import tarfile
+import bz2
+import gzip
 
 # code taken from https://stackoverflow.com/questions/15644964/python-progress-bar-and-downloads
 class DownloadProgressBar(tqdm):
@@ -29,7 +30,26 @@ if __name__ == '__main__':
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
     
-    url = f"https://dumps.wikimedia.org/other/enterprise_html/runs/{args.date}/{args.lang}wiki-NS{args.namespace}-{args.date}-ENTERPRISE-HTML.json.tar.gz"
-    output_path = f"{args.output_dir}/{args.lang}wiki-NS{args.namespace}-{args.date}-ENTERPRISE-HTML.json.tar.gz"
+    url_html_dump = f"https://dumps.wikimedia.org/other/enterprise_html/runs/{args.date}/{args.lang}wiki-NS{args.namespace}-{args.date}-ENTERPRISE-HTML.json.tar.gz"
+    output_html_path = f"{args.output_dir}/{args.lang}wiki-NS{args.namespace}-{args.date}-ENTERPRISE-HTML.json.tar.gz"
     
-    download_url(url, output_path)
+    url_xml_dump_1 = f"https://dumps.wikimedia.org/{args.lang}wiki/{args.date}/{args.lang}wiki-{args.date}-pages-articles.xml.bz2"
+    output_xml_path_1 = f"{args.output_dir}/{args.lang}wiki-{args.date}-pages-articles.xml.bz2"
+    
+    url_xml_dump_2 = f"https://dumps.wikimedia.org/{args.lang}wiki/{args.date}/{args.lang}wiki-{args.date}-page_props.sql.gz"
+    output_xml_path_2 = f"{args.output_dir}/{args.lang}wiki-{args.date}-page_props.sql.gz"
+    
+    # download_url(url_html_dump, output_html_path)
+    download_url(url_xml_dump_1, output_xml_path_1)
+    download_url(url_xml_dump_2, output_xml_path_2)
+    
+    # extract the downloaded files (xml and sql only)
+    with bz2.open(output_xml_path_1, 'rb') as f:
+        file_content = f.read()
+        with open(output_xml_path_1[:-4], 'wb') as f_out:
+            f_out.write(file_content)
+    
+    with gzip.open(output_xml_path_2, 'rb') as f:
+        file_content = f.read()
+        with open(output_xml_path_2[:-3], 'wb') as f_out:
+            f_out.write(file_content)
--- wikipedia_dumps_process/pages_extractor.py
+++ wikipedia_dumps_process/pages_extractor.py
@@ -1,85 +1,196 @@
 import argparse
+import bz2
+import gzip
 import json
 import os
+import tarfile
 import urllib.parse
-from glob import glob
+import xml.etree.ElementTree as ET
 
 import pandas as pd
 from tqdm import tqdm
 
 
 def process_title(title):
-    return urllib.parse.quote(title.replace(' ', '_'))
+    title = title.replace(' ', '_')
+    # decode from url encoding
+    title = urllib.parse.unquote(title)
+    # reencode into url encoding
+    title = urllib.parse.quote(title)
+    return title
 
 
-def extract_dump(df, language):
+def process_xml_articles(path, language):
+    # process xml
+    tree = ET.parse(path)
+    root = tree.getroot()
     pages = []
-    redirect_map = {}
-    for i in range(len(df)):
-        # get the page details
+    redirects = []
+    for child in tqdm(root):
         page_info = {}
-        page_info['title'] = process_title(df['name'][i])
-        page_info['ID'] = df['identifier'][i]
-        page_info['language'] = language
-        page_info['version'] = f"https://{language}.wikipedia.org/w/index.php?title={page_info['title']}&oldid={df['version'][i]['identifier']}"
-        if not pd.isna(df['main_entity'][i]):
-            page_info['QID'] = df['main_entity'][i]['identifier']
-        else:
-            page_info['QID'] = None
-        page_info['HTML'] = df['article_body'][i]['html']
-        pages.append(page_info)
+        redirect = {}
+        valid = True
+        if 'siteinfo' in child.tag:
+            continue
+        for grandchild in child:
+            # get the tag name without the http://www.mediawiki.org/xml/export-0.10/ prefix
+            tag = grandchild.tag.split('}')[1]
+            if tag == 'ns' and grandchild.text != '0':
+                valid = False
+                break
+            if tag == 'title':
+                page_info['title'] = process_title(grandchild.text)
+                redirect['title'] = process_title(grandchild.text)
+            if tag == 'id':
+                page_info['ID'] = grandchild.text
+                redirect['ID'] = grandchild.text
+            if tag == 'revision':
+                for greatgrandchild in grandchild:
+                    tag = greatgrandchild.tag.split('}')[1]
+                    if tag == 'id':
+                        page_info['version'] = greatgrandchild.text
+                        break
+            if tag == 'redirect':
+                redirect['redirect'] = grandchild.attrib['title']
+        if '1589' in page_info['title']:
+            print(page_info)
+        if 'redirect' in redirect:
+            redirects.append(redirect)
+        elif valid:
+            page_info['version'] = f"https://{language}.wikipedia.org/w/index.php?title={page_info['title']}&oldid={page_info['version']}"
+            pages.append(page_info)
+    return pages, redirects
 
-        # get the redirects
-        if type(pd.isna(df['redirects'][i])) != bool:
-            for redirect in df['redirects'][i]:
-                redirect_map[process_title(
-                    redirect['name'])] = page_info['title']
 
-    return pages, redirect_map
+def process_sql_data(path):
+    pages = []
+    with open(path, 'r', encoding='latin-1') as f:
+        data = f.readlines()
+        for line in tqdm(data):
+            if not line.startswith('INSERT INTO'):
+                continue
+            line = line.replace('INSERT INTO `page_props` VALUES ', '')
+            entries = line.split('),(')
+            entries[0] = entries[0].replace('(', '')
+            entries[-1] = entries[-1].replace(');', '')
+            for entry in entries:
+                try:
+                    id, category, qid, _ = entry.split(',', 3)
+                except:
+                    print(entry)
+                    continue
+                if category != "'wikibase_item'":
+                    continue
+                pages.append({'ID': id, 'QID': qid[1:-1]})
+    return pages
+
+
+def process_tar_data(data):
+    redirect_map = {}
+    page_info = {}
+    page_info['title'] = process_title(data['name'])
+    page_info['ID'] = data['identifier']
+    page_info['language'] = data['language']
+    page_info['version'] = f"https://{data['language']}.wikipedia.org/w/index.php?title={page_info['title']}&oldid={data['version']['identifier']}"
+    page_info['HTML'] = data['article_body']['html']
+    page_info['page_length'] = len(data['article_body']['html'])
+    if 'abstract' in data:
+        page_info['lead_paragraph'] = data['abstract']
+    else:
+        page_info['lead_paragraph'] = ''
+    if 'main_entity' in data:
+        page_info['QID'] = data['main_entity']['identifier']
+    else:
+        page_info['QID'] = None
+    if 'redirects' in data:
+        for redirect in data['redirects']:
+            redirect_map[process_title(
+                redirect['name'])] = page_info['title']
+
+    return page_info, redirect_map
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', type=str,
-                        required=True, help='Path to the data folder')
+    parser.add_argument('--input_xml', type=str,
+                        required=True, help='Path to the compressed xml input file')
+    parser.add_argument('--input_sql', type=str, required=True,
+                        help='Path to the compressed sql input file')
+    parser.add_argument('--input_tar', type=str, required=True,
+                        help='Path to the compressed tar input file')
     parser.add_argument('--language', type=str,
                         required=True, help='Language version of the Wikipedia dump')
     parser.add_argument('--output_dir', type=str,
                         required=True, help='Path to the output folder')
-    parser.add_argument('--chunksize', type=int, default=10_000,
-                        help='Chunksize for reading the json files')
     args = parser.parse_args()
 
-    # check if input dir exists
-    if not os.path.exists(args.input_dir):
-        raise ValueError(f"Input directory {args.input_dir} does not exist")
+    # check if input files exist
+    if not os.path.exists(args.input_xml):
+        raise ValueError(f"Input file {args.input_xml} does not exist")
+    if not os.path.exists(args.input_sql):
+        raise ValueError(f"Input file {args.input_sql} does not exist")
+    if not os.path.exists(args.input_tar):
+        raise ValueError(f"Input file {args.input_tar} does not exist")
     # check if output dir exists
     # if it doesn't exist, create it
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
-    # check if chunksize is positive
-    if args.chunksize <= 0:
-        raise ValueError(f"Chunksize must be positive, got {args.chunksize}")
 
-    # Read all json files
-    files = glob(f"{args.input_dir}/*.ndjson")
-    simple_pages = {}
-    redirect_map = {}
-    counter = 0
-    for file in files:
-        print(f"Processing {file}")
-        df = pd.read_json(file, chunksize=args.chunksize, lines=True)
-        for chunk in tqdm(df):
-            chunk = chunk.reset_index(drop=True)
-            pages, partial_redirect = extract_dump(chunk, args.language)
-            for page in pages:
-                simple_pages[page['title']] = {
-                    'ID': page['ID'], 'QID': page['QID']}
-            redirect_map.update(partial_redirect)
-            # create dataframe from pages
-            df = pd.DataFrame(pages)
-            df.to_parquet(f"{args.output_dir}/pages_{counter}.parquet")
-            counter += 1
+    # extract pages from xml dump
+    print(f"Parsing xml dump {args.input_xml}")
+    pages_xml, redirects = process_xml_articles(args.input_xml, args.language)
+    # extract pages from sql dump
+    print(f"Parsing sql dump {args.input_sql}")
+    pages_sql = process_sql_data(args.input_sql)
+
+    print("Saving DataFrames")
+    # create single dataframe for pages
+    df_xml = pd.DataFrame(pages_xml).set_index('ID')
+    df_sql = pd.DataFrame(pages_sql).set_index('ID')
+
+    pages = df_xml.merge(df_sql, left_index=True,
+                         right_index=True, how='inner')
+    pages.reset_index(level=0, inplace=True)
+    pages = pages.rename(columns={'index': 'ID'})
+
+    # create copy of pages with reduced information
+    simple_pages = pages[['ID', 'title']]
+    simple_pages = simple_pages.set_index('title')
+
+    # create dataframe for redirects
+    # use source page as index
+    df_redirects = pd.DataFrame(redirects).set_index('title')
+    # only keep the redirect column
+    df_redirects = df_redirects[['redirect']]
+
+    # save dataframes
+    pages.to_parquet(f"{args.output_dir}/pages.parquet")
+    simple_pages.to_parquet(f"{args.output_dir}/simple_pages.parquet")
+    df_redirects.to_parquet(f"{args.output_dir}/redirect_map.parquet")
+
+    print(f'Processing tar dump {args.input_tar}')
+    with tarfile.open(args.input_file, 'r:gz') as tar:
+        # iterate through files in tar
+        for member in (pbar := tqdm(tar)):
+            if member.name.endswith('json'):
+                full_pages = []
+                file_content = tar.extractfile(member.name).readlines()
+                for i, line in enumerate(file_content):
+                    if i % 1000 == 0:
+                        pbar.set_description(
+                            f"Processing file {member.name} at line {i}/{len(file_content)}")
+                    entry = json.loads(line)
+                    entry['language'] = args.language
+                    page, partial_redirect = extract_dump(entry)
+                    full_pages.append(page)
+                    simple_pages[page['title']] = {
+                        'ID': page['ID'], 'QID': page['QID']}
+                    redirect_map.update(partial_redirect)
+                # create dataframe from full_pages
+                df = pd.DataFrame(full_pages)
+                df.to_parquet(f"{args.output_dir}/pages_{counter}.parquet")
+                counter += 1
+
     # create dataframe from simple pages
     df = pd.DataFrame.from_dict(simple_pages, orient='index')
     df.to_parquet(f"{args.output_dir}/simple_pages.parquet")
