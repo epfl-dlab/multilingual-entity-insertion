d560210a61dec4d1e31aa8a1e6882f48ecd9cf36
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2023-10-04 11:59:21 +0200

    add freezing mechanisms to training script

--- data_modelling/training/main.py
+++ data_modelling/training/main.py
@@ -2,7 +2,6 @@ import argparse
 import os
 import time
 
-import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
@@ -20,9 +19,9 @@ def collator(input):
     sentences_1, sentences_2, sentences_3 = [], [], []
     labels = []
     for item in input:
-        sentences_1.append(item['sentence_1'])
-        sentences_2.append(item['sentence_2'])
-        sentences_3.append(item['sentence_3'])
+        sentences_1.append(item['source'])
+        sentences_2.append(item['context'])
+        sentences_3.append(item['target'])
         labels.append(item['label'])
     sentences_1 = tokenizer(sentences_1, padding=True,
                             truncation=True, return_tensors='pt')
@@ -34,7 +33,7 @@ def collator(input):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--model_name', type=str,
                         default='bert-base-uncased', help='Model name or path to model')
     parser.add_argument('--data_dir', type=str,
@@ -58,7 +57,11 @@ if __name__ == '__main__':
                         help='Resume training from checkpoint (needs --checkpoint_dir)')
     parser.add_argument('--checkpoint_dir', type=str,
                         help='Directory with checkpoint to resume training from')
-    parser.add_argument('--ga_steps', type=int, default=1, help='Number of steps for gradient accumulation')
+    parser.add_argument('--ga_steps', type=int, default=1,
+                        help='Number of steps for gradient accumulation')
+    parser.add_argument('--full_freeze_epochs', type=int, default=0,
+                        help='Number of epochs to freeze all lazyers except classification head')
+    parser.add_argument('--freeze_layers', type=int, default=0, help='Number of initial layers to freeze')
     parser.set_defaults(resume=False)
 
     args = parser.parse_args()
@@ -105,8 +108,10 @@ if __name__ == '__main__':
         logger.info(f"\t- {arg}: {getattr(args, arg)}")
 
     logger.info("Loading datasets")
-    train_set = WikiDataset('train')
-    val_set = WikiDataset('val')
+    train_set = WikiDataset(args.data_dir, 'train')
+    val_set = WikiDataset(args.data_dir, 'val')
+    logger.info(f"Train set size: {len(train_set)}")
+    logger.info(f"Validation set size: {len(val_set)}")
 
     logger.info("Creating dataloaders")
     train_loader = DataLoader(train_set,
@@ -149,7 +154,6 @@ if __name__ == '__main__':
         classification_head = torch.nn.Linear(model.config.hidden_size * 3, 2)
         tokenizer = AutoTokenizer.from_pretrained(args.model_name)
         tokenizer.save_pretrained(os.path.join(output_dir, 'tokenizer'))
-        model.to(device)
 
     logger.info("Initializing optimizer")
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
@@ -158,6 +162,20 @@ if __name__ == '__main__':
     # prepare all objects with accelerator
     model, classification_head, optimizer, train_loader, val_loader, scheduler = accelerator.prepare(
         model, classification_head, optimizer, train_loader, val_loader, scheduler)
+
+    if args.full_freeze_epochs > 0:
+        logger.info(f"Freezing all layers except classification head for {args.full_freeze_epochs} epochs")
+        for param in model.parameters():
+            param.requires_grad = False
+        for param in classification_head.parameters():
+            param.requires_grad = True
+    else:
+        logger.info(f"Freezing first {args.freeze_layers} layers")
+        for param in model.base_model.embeddings.parameters():  
+            param.requires_grad = False
+        for param in model.base_model.encoder.layer[:args.freeze_layers].parameters():
+            param.requires_grad = False
+
 
     logger.info("Starting training")
     step = 0
@@ -179,7 +197,8 @@ if __name__ == '__main__':
                 if step % args.print_step == 0:
                     logger.info(
                         f"Step {step}: loss = {running_loss / args.print_step}")
-                    writer.add_scalar('train/loss', running_loss / args.print_step, step)
+                    writer.add_scalar(
+                        'train/loss', running_loss / args.print_step, step)
                     running_loss = 0
                 accelerator.backward(loss)
                 optimizer.step()
@@ -192,8 +211,8 @@ if __name__ == '__main__':
                 # accelerator needs to unwrap model and classification head
                 accelerator.unwrap_model(model).save_pretrained(os.path.join(
                     output_dir, f"model_{step}"))
-                accelerator.unwrap_model(classification_head).save_pretrained(os.path.join(
-                    output_dir, f"classification_head_{step}"))
+                torch.save(accelerator.unwrap_model(classification_head).state_dict(), os.path.join(
+                    output_dir, f"classification_head_{step}.pth"))
 
             # evaluate model
             if step % args.eval_step == 0:
@@ -209,30 +228,40 @@ if __name__ == '__main__':
                     for val_data in val_loader:
                         val_embeddings = []
                         for input in val_data['inputs']:
-                            output = model(input['input_ids'], input['attention_mask'])
-                            val_embeddings.append(output['last_hidden_state'][:, 0, :])
+                            output = model(
+                                input['input_ids'], input['attention_mask'])
+                            val_embeddings.append(
+                                output['last_hidden_state'][:, 0, :])
                         val_embeddings = torch.cat(val_embeddings, dim=1)
                         val_logits = classification_head(val_embeddings)
-                        val_loss = F.cross_entropy(val_logits, val_data['labels'])
+                        val_loss = F.cross_entropy(
+                            val_logits, val_data['labels'])
                         running_val_loss += val_loss.item()
-                        
+
                         # measure true positives, true negatives, false positives, false negatives
                         # use softmax to get probabilities
                         labels = val_data['labels']
                         probs = torch.softmax(val_logits, dim=1)
                         preds = torch.argmax(probs, dim=1)
 
-                        true_pos += torch.sum((preds == 1) & (labels == 1)).item()
-                        true_neg += torch.sum((preds == 0) & (labels == 0)).item()
-                        false_pos += torch.sum((preds == 1) & (labels == 0)).item()
-                        false_neg += torch.sum((preds == 0) & (labels == 1)).item()
+                        true_pos += torch.sum((preds == 1)
+                                              & (labels == 1)).item()
+                        true_neg += torch.sum((preds == 0)
+                                              & (labels == 0)).item()
+                        false_pos += torch.sum((preds == 1)
+                                               & (labels == 0)).item()
+                        false_neg += torch.sum((preds == 0)
+                                               & (labels == 1)).item()
                         total += len(labels)
 
                     # calculate accuracy, precision, recall, f1 score
                     accuracy = (true_pos + true_neg) / total
-                    precision = true_pos / (true_pos + false_pos)
-                    recall = true_pos / (true_pos + false_neg)
-                    f1 = 2 * precision * recall / (precision + recall)
+                    precision = true_pos / \
+                        (true_pos + false_pos) if true_pos + false_pos > 0 else 0
+                    recall = true_pos / \
+                        (true_pos + false_neg) if true_pos + false_neg > 0 else 0
+                    f1 = 2 * precision * recall / \
+                        (precision + recall) if precision + recall > 0 else 0
                     running_val_loss /= len(val_loader)
                     logger.info(f"Accuracy: {accuracy}")
                     logger.info(f"Precision: {precision}")
@@ -247,8 +276,17 @@ if __name__ == '__main__':
                 model.train()
 
         scheduler.step()
+        
+        # unfreeze model if necessary
+        if epoch + 1 == args.full_freeze_epochs:
+            logger.info(f"Unfreezing model except first {args.freeze_layers} layers")
+            for param in model.parameters():
+                param.requires_grad = True
+            for param in model.base_model.embeddings.parameters():
+                param.requires_grad = False
+            for param in model.base_model.encoder.layer[:args.freeze_layers].parameters():
+                param.requires_grad = False
 
     # close logger
     logger.info("Training finished")
     writer.close()
-    
