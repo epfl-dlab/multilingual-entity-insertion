b548151d85c4068e4d5c077055ae7f1b97bf0d55
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2023-10-04 11:59:35 +0200

    set up dataloader

--- data_modelling/training/data.py
+++ data_modelling/training/data.py
@@ -1,5 +1,29 @@
+import os
+from glob import glob
+
+import pandas as pd
 import torch.utils.data as data
 
+
 class WikiDataset(data.Dataset):
-    def __init__():
-        pass
+    def __init__(self, data_dir, split):
+        super(WikiDataset, self).__init__()
+        self.data = self.get_data(data_dir, split)
+
+    def get_data(self, data_dir, split):
+        data_dir = os.path.join(data_dir, split)
+        files = glob(os.path.join(data_dir, '*.parquet'))
+        dfs = []
+        for file in files:
+            df = pd.read_parquet(file)
+            dfs.append(df)
+        df = pd.concat(dfs)
+        return df
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        return self.data.iloc[index].to_dict()
+
+    
