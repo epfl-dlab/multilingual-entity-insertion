65fb315ae3a8889d08f92033468771d5aa831359
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2023-10-24 15:58:20 +0200

    make dataloader more general

--- data_modelling/training_ranking/data.py
+++ data_modelling/training_ranking/data.py
@@ -8,11 +8,11 @@ import torch.utils.data as data
 class WikiDataset(data.Dataset):
     def __init__(self, data_dir, split, neg_samples):
         super(WikiDataset, self).__init__()
-        self.data = self.get_data(data_dir, split)
+        self.data = self.get_data(data_dir, split, neg_samples)
         self.split = split
         self.neg_samples = neg_samples
 
-    def get_data(self, data_dir, split):
+    def get_data(self, data_dir, split, neg_samples):
         data_dir = os.path.join(data_dir, split)
         files = glob(os.path.join(data_dir, '*.parquet'))
         dfs = []
@@ -23,10 +23,17 @@ class WikiDataset(data.Dataset):
         # negative columns have "_neg_{i}" suffix
         # remove all columns with i >= neg_samples
         remove_cols = []
+        max_found_col = 0
         for column in df:
             if '_neg_' in column:
-                if int(column.split('_')[-1]) >= self.neg_samples:
+                if int(column.split('_')[-1]) >= neg_samples:
                     remove_cols.append(column)
+                else:
+                    max_found_col = max(
+                        max_found_col, int(column.split('_')[-1]))
+        if max_found_col < neg_samples - 1:
+            raise ValueError(
+                f'Not enough negative samples found. Found {max_found_col}, required {neg_samples}')
         df = df.drop(remove_cols, axis=1)
         return df
 
