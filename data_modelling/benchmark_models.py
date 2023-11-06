2eb4319b581ca1b21a722c7a3a49e7a8e9ee64dc
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2023-11-06 12:34:40 +0100

    separete bechmarking script into baselines and models

--- data_modelling/benchmark_models.py
+++ data_modelling/benchmark_models.py
@@ -9,33 +9,76 @@ import os
 from urllib import parse
 from tqdm import tqdm
 import random
+from glob import glob
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from baselines import bm25
-from baselines import exact_match
-from baselines import fuzzy_match
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', type=str, required=True)
-    parser.add_argument('--mention_map_month_1', type=str, required=True)
-    parser.add_argument('--mention_map_month_2', type=str, required=True)
+    parser.add_argument('--data_path', type=str,
+                        required=True, help='Path to data file')
+    parser.add_argument('--models_dir', type=str, required=True,
+                        help='Directory containing model directories')
+    parser.add_argument('--mention_map', type=str,
+                        required=True, help='Path to mention map file')
     parser.add_argument('--data_limit', type=int, default=None,
                         help='Limit the number of rows to use')
-    parser.add_argument('--method_name', type=str, required=True, choices=[
-                        'random', 'bm25', 'bm25_mentions', 'exact_match', 'fuzzy_match', 'model_rank_corruption', 'model_rank_no_corruption', 'model_random_section', 'all'], help='Which method to use')
-    
+    parser.add_argument('--column_name', type=str, required=True, help='Name of column to add to dataframe')
+    parser.add_argument('--loss_function', type=str, required=True, choices=[
+                        'ranking', 'indep'], help='Selected loss function used for training')
+    parser.add_argument('--use_corruption', action='store_true',
+                        help='Use model trained with corruption')
+    parser.add_argument('--use_section_title',
+                        action='store_true', help='Use section title in input')
+    parser.add_argument('--use_section_title_random',
+                        action='store_true', help='Use random section title in input')
+    parser.add_argument('--use_mentions', action='store_true',
+                        help='Use mentions in input')
+    parser.set_defaults(use_corruption=False, use_section_title=False,
+                        use_section_title_random=False, use_mentions=False)
+
     args = parser.parse_args()
 
     # check if data path exists
     if not os.path.exists(args.data_path):
         raise ValueError('Data path does not exist')
-    # check if mention map month 1 exists
-    if not os.path.exists(args.mention_map_month_1):
-        raise ValueError('Mention map month 1 does not exist')
-    # check if mention map month 2 exists
-    if not os.path.exists(args.mention_map_month_2):
-        raise ValueError('Mention map month 2 does not exist')
+    # check if mention map exists
+    if not os.path.exists(args.mention_map):
+        raise ValueError('Mention map does not exist')
+
+    # find model name
+    model_name = args.loss_function
+    if args.use_corruption:
+        model_name += '_corrupt'
+    if args.use_section_title or args.use_section_title_random:
+        model_name += '_section'
+    if args.use_mentions:
+        model_name += '_mentions'
+
+    # load model
+    dir = os.path.join(args.models_dir, model_name)
+    model_path = glob(os.path.join(dir, 'model_*'))[0]
+    classification_head_path = glob(
+        os.path.join(dir, 'classification_head_*'))[0]
+    tokenizer_path = os.path.join(dir, 'tokenizer')
+
+    model = AutoModel.from_pretrained(model_path)
+    model.eval()
+    if args.loss_function == 'ranking':
+        classification_head = nn.Sequential(nn.Linear(model.config.hidden_size * 3, model.config.hidden_size),
+                                            nn.ReLU(),
+                                            nn.Linear(model.config.hidden_size, 1))
+    else:
+        classification_head = nn.Sequential(nn.Linear(model.config.hidden_size * 3, model.config.hidden_size),
+                                            nn.ReLU(),
+                                            nn.Linear(model.config.hidden_size, 2))
+    classification_head.load_state_dict(torch.load(
+        classification_head_path, map_location=torch.device('cpu')))
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+
+    model = model.to('cuda' if torch.cuda.is_available() else 'cpu')
+    classification_head = classification_head.to(
+        'cuda' if torch.cuda.is_available() else 'cpu')
 
     # load data
     df = pd.read_parquet(args.data_path)
@@ -59,8 +102,7 @@ if __name__ == '__main__':
                 all_sections.add(source_section)
     all_sections = list(all_sections)
 
-    mention_map_pre = pd.concat([pd.read_parquet(args.mention_map_month_1), pd.read_parquet(
-        args.mention_map_month_2)]).drop_duplicates().reset_index(drop=True)
+    mention_map_pre = pd.read_parquet(args.mention_map)
     mention_map_pre = mention_map_pre.to_dict('records')
     mention_map = {}
     for row in mention_map_pre:
@@ -69,180 +111,47 @@ if __name__ == '__main__':
             mention_map[title].append(row['mention'])
         else:
             mention_map[title] = [row['mention']]
-            
-    if args.method_name == 'all':
-        args.method_name = ['random', 'bm25', 'bm25_mentions', 'exact_match', 'fuzzy_match', 'model_rank_corruption', 'model_rank_no_corruption', 'model_random_section']
-    else:
-        args.method_name = [args.method_name]
 
-    if 'random' in args.method_name:
-        print('Calculating random ranks')
-        rank = []
-        for context in tqdm(contexts):
-            position = random.randint(1, len(context))
-            rank.append(position)
-        df['random_rank'] = rank
-    if 'bm25' in args.method_name:
-        print('Calculating bm25 ranks')
-        rank = []
-        for context, title, lead in tqdm(zip(contexts, target_titles, target_leads), total=len(target_titles)):
-            scores = bm25.rank_contexts(context, title, lead)
-            position = 1
-            for score in scores[1:]:
-                if score > scores[0]:
-                    position += 1
-            rank.append(position)
-        df['bm25_rank'] = rank
-    if 'bm25_mentions' in args.method_name:
-        print('Calculation bm25 with mention knowledge')
-        rank = []
-        for context, title, lead in tqdm(zip(contexts, target_titles, target_leads), total=len(target_titles)):
-            scores = bm25.rank_contexts(context, title, lead, mention_map[title])
-            position = 1
-            for score in scores[1:]:
-                if score > scores[0]:
-                    position += 1
-            rank.append(position)
-        df['bm25_mentions_rank'] = rank
-    if 'exact_match' in args.method_name:
-        print('Calculating exact match ranks')
-        rank = []
-        for context, title, lead in tqdm(zip(contexts, target_titles, target_leads), total=len(target_titles)):
-            scores = exact_match.rank_contexts(context, mention_map[title])
-            position = 1
-            equals = 0
-            for score in scores[1:]:
-                if score > scores[0]:
-                    position += 1
-                if score == scores[0]:
-                    equals += 1
-            rank.append(position + random.randint(0, equals))
-        df['exact_match_rank'] = rank
-    if 'fuzzy_match' in args.method_name:
-        print('Calculating fuzzy match ranks')
-        rank = []
-        for context, title, lead in tqdm(zip(contexts, target_titles, target_leads), total=len(target_titles)):
-            scores = fuzzy_match.rank_contexts(context, mention_map[title])
-            position = 1
-            equals = 0
-            for score in scores[1:]:
-                if score > scores[0]:
-                    position += 1
-                if score == scores[0]:
-                    equals += 1
-            rank.append(position + random.randint(0, equals))
-        df['fuzzy_match_rank'] = rank
-    if 'model_rank_corruption' in args.method_name:
-        print('Calculating model with ranking losses and corruption ranks')
-        model_path = 'training_ranking/models/listsoftmax_corruption/model/'
-        tokenizer_path = 'training_ranking/models/listsoftmax_corruption/tokenizer/'
-        classification_head_path = 'training_ranking/models/listsoftmax_corruption/classification_head.pth'
-        column_name = 'model_ranking_corruption_rank'
-        model = AutoModel.from_pretrained(model_path)
-        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        classification_head = nn.Sequential(nn.Linear(model.config.hidden_size * 3, model.config.hidden_size),
-                                            nn.ReLU(),
-                                            nn.Linear(model.config.hidden_size, 1))
-        classification_head.load_state_dict(torch.load(classification_head_path, map_location=torch.device('cpu')))
-        model.eval()
-
-        model = model.to('cuda' if torch.cuda.is_available() else 'cpu')
-        classification_head = classification_head.to('cuda' if torch.cuda.is_available() else 'cpu')
-
-        rank = []
-        with torch.no_grad():
-            for context, source_section, source_title, source_lead, target_title, target_lead in tqdm(zip(contexts, source_sections, source_titles, source_leads, target_titles, target_leads), total=len(target_titles)):
-                source = tokenizer([f"{source_title}{tokenizer.sep_token}{source_lead}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                target = tokenizer([f"{target_title}{tokenizer.sep_token}{target_lead}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                source_embeddings = model(**source)['last_hidden_state'][:, 0, :]
-                target_embeddings = model(**target)['last_hidden_state'][:, 0, :]
-                scores = []
-                for c, s in zip(context, source_section):
-                    input = tokenizer([f"{s}{tokenizer.sep_token}{c}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                    input_embeddings = model(**input)['last_hidden_state'][:, 0, :]
-                    input = torch.cat((source_embeddings, input_embeddings, target_embeddings), dim=1)
+    print(f'Calculating model rankings. Model type: {model_name}')
+    rank = []
+    with torch.no_grad():
+        for context, source_section, source_title, source_lead, target_title, target_lead in tqdm(zip(contexts, source_sections, source_titles, source_leads, target_titles, target_leads), total=len(target_titles)):
+            if target_title not in mention_map:
+                mention_map[target_title] = []
+            source = tokenizer([f"{source_title}{tokenizer.sep_token}{source_lead}"], return_tensors='pt',
+                                 padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
+            target = tokenizer([f"{target_title}{tokenizer.sep_token}{target_lead}"], return_tensors='pt',
+                                    padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
+            source_embeddings = model(
+                **source)['last_hidden_state'][:, 0, :]
+            target_embeddings = model(
+                **target)['last_hidden_state'][:, 0, :]
+            scores = []
+            for c, s in zip(context, source_section):
+                input = ''
+                if args.use_section_title or args.use_section_title_random:
+                    if args.use_section_title_random:
+                        s = random.choice(all_sections)
+                    input += f"{s}{tokenizer.sep_token}"
+                if args.use_mentions:
+                    input += f"{' '.join(mention_map[target_title])}{tokenizer.sep_token}"
+                input += f"{c}"
+                input = tokenizer([input], return_tensors='pt', padding=True,
+                                    truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
+                input_embeddings = model(
+                    **input)['last_hidden_state'][:, 0, :]
+                input = torch.cat(
+                    (source_embeddings, input_embeddings, target_embeddings), dim=1)
+                if args.loss_function == 'ranking':
                     score = classification_head(input)[0].squeeze().item()
-                    scores.append(score)
-                position = 1
-                for score in scores[1:]:
-                    if score > scores[0]:
-                        position += 1
-                rank.append(position)
-        df[column_name] = rank
-    if 'model_rank_no_corruption' in args.method_name:
-        print('Calculating model with ranking losses and no corruption ranks')
-        model_path = 'training_ranking/models/listsoftmax_no_corruption/model/'
-        tokenizer_path = 'training_ranking/models/listsoftmax_no_corruption/tokenizer/'
-        classification_head_path = 'training_ranking/models/listsoftmax_no_corruption/classification_head.pth'
-        column_name = 'model_ranking_no_corruption_rank'
-        model = AutoModel.from_pretrained(model_path)
-        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        classification_head = nn.Sequential(nn.Linear(model.config.hidden_size * 3, model.config.hidden_size),
-                                            nn.ReLU(),
-                                            nn.Linear(model.config.hidden_size, 1))
-        classification_head.load_state_dict(torch.load(classification_head_path, map_location=torch.device('cpu')))
-        model.eval()
-
-        model = model.to('cuda' if torch.cuda.is_available() else 'cpu')
-        classification_head = classification_head.to('cuda' if torch.cuda.is_available() else 'cpu')
-
-        rank = []
-        with torch.no_grad():
-            for context, source_section, source_title, source_lead, target_title, target_lead in tqdm(zip(contexts, source_sections, source_titles, source_leads, target_titles, target_leads), total=len(target_titles)):
-                source = tokenizer([f"{source_title}{tokenizer.sep_token}{source_lead}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                target = tokenizer([f"{target_title}{tokenizer.sep_token}{target_lead}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                source_embeddings = model(**source)['last_hidden_state'][:, 0, :]
-                target_embeddings = model(**target)['last_hidden_state'][:, 0, :]
-                scores = []
-                for c, s in zip(context, source_section):
-                    input = tokenizer([f"{s}{tokenizer.sep_token}{c}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                    input_embeddings = model(**input)['last_hidden_state'][:, 0, :]
-                    input = torch.cat((source_embeddings, input_embeddings, target_embeddings), dim=1)
-                    score = classification_head(input)[0].squeeze().item()
-                    scores.append(score)
-                position = 1
-                for score in scores[1:]:
-                    if score > scores[0]:
-                        position += 1
-                rank.append(position)
-        df[column_name] = rank
-    if 'model_random_section' in args.method_name:
-        print('Calculating model with ranking losses and corruption ranks, but using a random section name')
-        model_path = 'training_ranking/models/listsoftmax_corruption/model/'
-        tokenizer_path = 'training_ranking/models/listsoftmax_corruption/tokenizer/'
-        classification_head_path = 'training_ranking/models/listsoftmax_corruption/classification_head.pth'
-        column_name = 'model_random_section_rank'
-        model = AutoModel.from_pretrained(model_path)
-        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        classification_head = nn.Sequential(nn.Linear(model.config.hidden_size * 3, model.config.hidden_size),
-                                            nn.ReLU(),
-                                            nn.Linear(model.config.hidden_size, 1))
-        classification_head.load_state_dict(torch.load(classification_head_path, map_location=torch.device('cpu')))
-        model.eval()
-
-        model = model.to('cuda' if torch.cuda.is_available() else 'cpu')
-        classification_head = classification_head.to('cuda' if torch.cuda.is_available() else 'cpu')
-
-        rank = []
-        with torch.no_grad():
-            for context, source_section, source_title, source_lead, target_title, target_lead in tqdm(zip(contexts, source_sections, source_titles, source_leads, target_titles, target_leads), total=len(target_titles)):
-                source = tokenizer([f"{source_title}{tokenizer.sep_token}{source_lead}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                target = tokenizer([f"{target_title}{tokenizer.sep_token}{target_lead}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                source_embeddings = model(**source)['last_hidden_state'][:, 0, :]
-                target_embeddings = model(**target)['last_hidden_state'][:, 0, :]
-                scores = []
-                for c, s in zip(context, source_section):
-                    s = random.choice(all_sections)
-                    input = tokenizer([f"{s}{tokenizer.sep_token}{c}"], return_tensors='pt', padding=True, truncation=True, max_length=256).to('cuda' if torch.cuda.is_available() else 'cpu')
-                    input_embeddings = model(**input)['last_hidden_state'][:, 0, :]
-                    input = torch.cat((source_embeddings, input_embeddings, target_embeddings), dim=1)
-                    score = classification_head(input)[0].squeeze().item()
-                    scores.append(score)
-                position = 1
-                for score in scores[1:]:
-                    if score > scores[0]:
-                        position += 1
-                rank.append(position)
-        df[column_name] = rank
-    
+                else:
+                    score = classification_head(input)[0].squeeze().tolist()[1]
+                scores.append(score)
+            position = 1
+            for score in scores[1:]:
+                if score > scores[0]:
+                    position += 1
+            rank.append(position)
+        
+    df[args.column_name] = rank
     df.to_parquet('test_ranking_scores.parquet')
