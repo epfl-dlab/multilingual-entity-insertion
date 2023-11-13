b2b8e878709ad23638a5185cdd25dcdf2bd43294
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2023-11-13 18:06:59 +0100

    improve string match-based baselines

--- data_modelling/baselines/bm25.py
+++ data_modelling/baselines/bm25.py
@@ -1,14 +1,27 @@
 from rank_bm25 import BM25Okapi
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+import string
 
 def rank_contexts(contexts, target_title, target_lead, mentions = None):
-    tokenized_contexts = [context.lower().split(" ") for context in contexts]
+    stop_words = set(stopwords.words('english'))
+    punctuation = '!"#&\'()*+,./:;<=>?@[\\]^_`{|}~'
+    tokenized_contexts = [word_tokenize(context.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for context in contexts]
+    tokenized_contexts = [[word for word in context if word not in stop_words] for context in tokenized_contexts]
+    # tokenized_contexts = [context.lower().replace('\n', ' ').split(" ") for context in contexts]
     bm25 = BM25Okapi(tokenized_contexts)
+    # tokenized_target_lead = word_tokenize(target_title.lower().replace('\n', ' ').translate(str.maketrans('', '', string.punctuation)))
     if mentions is None:
-        tokenized_query = target_title.lower().split(" ") + target_lead.lower().split(" ")
+        tokenized_query = [target_title.lower()]
+        # tokenized_query = [target_title.lower()]
     else:
-        mentions = [mention.lower().split(" ") for mention in mentions]
+        # mentions = [mention.lower().split(" ") for mention in mentions]
+        # mentions = [word for mention in mentions for word in mention]
+        # tokenized_query = target_title.lower().split(" ") + target_lead.lower().replace('\n', ' ').split(" ") + mentions
+        mentions = [word_tokenize(mention.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for mention in mentions]
         mentions = [word for mention in mentions for word in mention]
-        tokenized_query = target_title.lower().split(" ") + target_lead.lower().split(" ") + mentions
+        tokenized_query = [target_title.lower()] + mentions
+        tokenized_query = [word for word in tokenized_query if word not in stop_words]
 
     scores = bm25.get_scores(tokenized_query)
     return scores
--- data_modelling/baselines/fuzzy_match.py
+++ data_modelling/baselines/fuzzy_match.py
@@ -1,13 +1,22 @@
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
 def rank_contexts(contexts, target_mentions):
+    stop_words = set(stopwords.words('english'))
+    punctuation = '!"#&\'()*+,./:;<=>?@[\\]^_`{|}~'
+    tokenized_contexts = [word_tokenize(context.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for context in contexts]
+    tokenized_contexts = [[word for word in context if word not in stop_words] for context in tokenized_contexts]
+    tokenized_mentions = [word_tokenize(mention.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for mention in target_mentions]
+    tokenized_mentions = [[word for word in mention if word not in stop_words] for mention in tokenized_mentions]
+    tokenized_mentions = [word for mention in tokenized_mentions for word in mention]
+    
     scores = []
     freqs = {}
-    for mention in target_mentions:
-        words = [word.lower() for word in mention.split()]
-        for word in words:
-            freqs[word] = freqs.get(word, 0) + 1
-    for context in contexts:
+    for word in tokenized_mentions:
+        freqs[word] = freqs.get(word, 0) + 1
+    for context in tokenized_contexts:
         score = 0
-        for word in context.split():
-            score += freqs.get(word.lower(), 0)
+        for word in context:
+            score += freqs.get(word, 0)
         scores.append(score / len(context))
     return scores
