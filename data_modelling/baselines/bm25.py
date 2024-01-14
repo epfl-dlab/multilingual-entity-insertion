4257a69268f3875c380108f7ac880d8f0ea62d1b
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2024-01-14 12:21:33 +0100

    add japanese tokenizer to baselines

--- data_modelling/baselines/bm25.py
+++ data_modelling/baselines/bm25.py
@@ -2,19 +2,37 @@ from rank_bm25 import BM25Okapi
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 import string
+import MeCab
 
-def rank_contexts(contexts, target_title, target_lead, mentions = None):
-    stop_words = set(stopwords.words('english'))
+def ja_tokenize(text):
+    wakati = MeCab.Tagger("-Owakati")
+    tokens = wakati.parse(text).split()
+    return tokens    
+
+def rank_contexts(contexts, target_title, target_lead, mentions = None, use_stopwords = True, use_japanese = False):
+    if use_stopwords:
+        stop_words = set(stopwords.words('english'))
+    else:
+        stop_words = []
     punctuation = '!"#&\'()*+,./:;<=>?@[\\]^_`{|}~'
-    tokenized_contexts = [word_tokenize(context.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for context in contexts]
+    if use_japanese:
+        tokenized_contexts = [ja_tokenize(context.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for context in contexts]
+    else:
+        tokenized_contexts = [word_tokenize(context.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for context in contexts]
     tokenized_contexts = [[word for word in context if word not in stop_words] for context in tokenized_contexts]
     bm25 = BM25Okapi(tokenized_contexts)
-    tokenized_target_lead = word_tokenize(target_lead.lower().replace('\n', ' ').translate(str.maketrans('', '', string.punctuation)))
+    if use_japanese:
+        tokenized_target_lead = ja_tokenize(target_lead.lower().replace('\n', ' ').translate(str.maketrans('', '', string.punctuation)))
+    else:
+        tokenized_target_lead = word_tokenize(target_lead.lower().replace('\n', ' ').translate(str.maketrans('', '', string.punctuation)))
     tokenized_target_lead = [word for word in tokenized_target_lead if word not in stop_words]
     if mentions is None:
         tokenized_query = [target_title.lower()] + tokenized_target_lead
     else:
-        mentions = [word_tokenize(mention.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for mention in mentions]
+        if use_japanese:
+            mentions = [ja_tokenize(mention.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for mention in mentions]
+        else:
+            mentions = [word_tokenize(mention.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for mention in mentions]
         mentions = [word for mention in mentions for word in mention if word not in stop_words]        
         tokenized_query = [target_title.lower()] + mentions + tokenized_target_lead
 
--- data_modelling/baselines/fuzzy_match.py
+++ data_modelling/baselines/fuzzy_match.py
@@ -1,15 +1,31 @@
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+import MeCab
 
-def rank_contexts(contexts, target_mentions):
-    stop_words = set(stopwords.words('english'))
+def ja_tokenize(text):
+    wakati = MeCab.Tagger("-Owakati")
+    tokens = wakati.parse(text).split()
+    return tokens    
+
+
+def rank_contexts(contexts, target_mentions, use_stopwords = True, use_japanese = False):
+    if use_stopwords:
+        stop_words = set(stopwords.words('english'))
+    else:
+        stop_words = []
     punctuation = '!"#&\'()*+,./:;<=>?@[\\]^_`{|}~'
-    tokenized_contexts = [word_tokenize(context.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for context in contexts]
+    if use_japanese:
+        tokenized_contexts = [ja_tokenize(context.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for context in contexts]
+    else:
+        tokenized_contexts = [word_tokenize(context.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for context in contexts]
     tokenized_contexts = [[word for word in context if word not in stop_words] for context in tokenized_contexts]
-    tokenized_mentions = [word_tokenize(mention.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for mention in target_mentions]
+    if use_japanese:
+        tokenized_mentions = [ja_tokenize(mention.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for mention in target_mentions]
+    else:
+        tokenized_mentions = [word_tokenize(mention.lower().replace('\n', ' ').translate(str.maketrans(punctuation, ' '*len(punctuation)))) for mention in target_mentions]
     tokenized_mentions = [[word for word in mention if word not in stop_words] for mention in tokenized_mentions]
     tokenized_mentions = [word for mention in tokenized_mentions for word in mention]
-    
+
     scores = []
     freqs = {}
     for word in tokenized_mentions:
